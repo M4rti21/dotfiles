bb71fa940d3f5c0afc13f8bb45a7ad6b79a967ec
M4rti21 <m4rti21@proton.me>
2024-02-18 22:51:12 +0100

    list was with get lmao

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-starship init fish | source
+set -U fish_greeting
+
+zoxide init fish | source
 
 alias ls="exa --all --long --icons --group-directories-first"
+alias cd="z"
 alias update="echo 'pacman' && sudo pacman -Syu && echo 'aur' && yay -Syu && echo 'flatpak' && flatpak update"
+alias nf="neofetch"
+alias vim="nvim"
+alias t="tmux"
+alias ta="tmux a"
+alias tat="tmux a -t"
