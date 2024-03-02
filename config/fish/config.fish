c2ec463e24504c9940abb26d5a1ed92e64f78e22
M4rti21 <m4rti21@proton.me>
2024-03-02 18:28:37 +0100

    sorted everything

---
+++ config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -U fish_greeting
+
+zoxide init fish | source
+
+alias cd="z"
+alias ls="exa --long --icons --group-directories-first"
+alias la="exa --long --icons --group-directories-first --all"
+alias update="echo 'pacman' && sudo pacman -Syu && echo 'aur' && yay -Syu && echo 'flatpak' && flatpak update"
+alias purge="sudo pacman -Qdtq | sudo pacman -Rns - && flatpak uninstall --unused -y"
+alias nf="neofetch"
+alias vim="nvim"
+alias t="tmux"
+alias ta="tmux a"
+alias tn="tmux new -s"
+alias tl="tmux ls"
+alias tk="tmux kill-session -t"
+alias tat="tmux a -t"
+
---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,76 @@
+return {
+
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "stevearc/conform.nvim",
+        "terrortylor/nvim-comment",
+    },
+
+    config = function()
+        local capabilities = require('cmp_nvim_lsp').default_capabilities();
+        require("mason").setup()
+        require("mason-lspconfig").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "tsserver",
+            },
+            automatic_installation = true,
+            handlers = {
+                -- default handler
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        capabilities = capabilities
+                    })
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup({
+                        capabilities = capabilities,
+                        settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+                    })
+                end,
+            },
+        })
+
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "isort", "black" },
+                javascript = { "deno" },
+                typescript = { "deno" },
+                bash = { "beautysh" },
+                css = { "stylelint" },
+                java = { "astyle" },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+        })
+
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                require("conform").format({ bufnr = args.buf })
+            end,
+        })
+
+        vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+        require("nvim_comment").setup({
+            -- Linters prefer comment and line to have a space in between markers
+            marker_padding = true,
+            comment_empty = true,
+            comment_empty_trim_whitespace = true,
+            create_mappings = true,
+            line_mapping = "gcc",
+            operator_mapping = "gc",
+            comment_chunk_text_object = "ic",
+            hook = nil,
+        })
+    end
+
+}
