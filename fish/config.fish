b14b774db7d16a0141a9ee2ef4d25aaf2c097e15
M4rti21 <m4rti21@proton.me>
2024-03-02 12:41:56 +0100

    try to fix languages

--- fish/config.fish
+++ fish/config.fish
@@ -6,13 +6,17 @@ set -U fish_greeting
 
 zoxide init fish | source
 
+alias cd="z"
 alias ls="exa --long --icons --group-directories-first"
 alias la="exa --long --icons --group-directories-first --all"
-alias cd="z"
 alias update="echo 'pacman' && sudo pacman -Syu && echo 'aur' && yay -Syu && echo 'flatpak' && flatpak update"
-alias purge="sudo pacman -Qdtq | sudo pacman -Rns - "
+alias purge="sudo pacman -Qdtq | sudo pacman -Rns -"
 alias nf="neofetch"
 alias vim="nvim"
 alias t="tmux"
 alias ta="tmux a"
+alias tn="tmux new -s"
+alias tl="tmux ls"
+alias tk="tmux kill-session -t"
 alias tat="tmux a -t"
+
