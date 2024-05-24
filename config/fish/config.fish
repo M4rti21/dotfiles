0700bd83a1a8c0fcfd1043494f0b6b6a3e4e1761
M4rti21 <m4rti21@proton.me>
2024-05-24 14:45:48 +0200

    lots of things

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,26 +6,42 @@ set -U fish_greeting
 
 # PATH
 set -x  PATH $PATH
-set -gx PATH /usr/bin         $PATH
 set -gx PATH /usr/sbin        $PATH
-set -gx PATH /usr/local/bin   $PATH
+set -gx PATH /usr/bin         $PATH
 set -gx PATH /usr/local/sbin  $PATH
+set -gx PATH /usr/local/bin   $PATH
 set -gx PATH $HOME/.local/bin $PATH
 
 bind \cf 'tmux-sessionizer'
 
 # VARIABLES
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+# Home paths
+export XCURSOR_PATH=/usr/share/icons:$XDG_DATA_HOME/icons
+
+export HISTFILE="$XDG_STATE_HOME/bash/history"
+export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
+export GOPATH="$XDG_DATA_HOME/go"
+export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"
+export GNUPGHOME="$XDG_DATA_HOME/gnupg"
+export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export ICEAUTHORITY="$XDG_CACHE_HOME"/ICEauthority
+
+alias wget="wget --hsts-file=$XDG_DATA_HOME/wget-hsts"
+# export MYVIMRC="$XDG_CONFIG_HOME/.vimrc"
+# export VIMINIT="source $MYVIMRC"
+
 export EDITOR=nvim
 export BROWSER=firefox
-export GOPATH="$HOME/.local/share/go"
-export __GL_SHADER_DISK_CACHE_PATH="$HOME/.local/share/nv"
-export NUGET_PACKAGES="$HOME/.local/share/nuget"
-export CARGO_HOME="$HOME/.local/share/cargo"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export NUGET_PACKAGES="$XDG_DATA_HOME/nuget"
+export __GL_SHADER_DISK_CACHE_PATH="$XDG_DATA_HOME/nv"
 
 # do this to make sure that the path is set correctly
-export ANDROID_HOME="/opt/android-sdk"
-export ANDROID_SDK_ROOT="/opt/android-sdk"
-
 alias rm="trash-put"
 alias rmls="trash-list"
 alias fm="joshuto"
