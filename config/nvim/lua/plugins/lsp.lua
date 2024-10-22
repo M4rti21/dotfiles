b2e1bf6b1308cc7b637a7c9ec2c9eb4c295cfcc4
M4rti21 <m4rti21@proton.me>
2024-10-22 12:58:00 +0200

    stuff?

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -13,9 +13,7 @@ return {
         }
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-            },
+            ensure_installed = { "lua_ls" },
             automatic_installation = true,
             handlers = {
                 function(server_name)
