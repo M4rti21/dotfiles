c713ba999a2e936862480c7726e2a6c6113d096d
M4rti21 <m4rti21@proton.me>
2024-10-23 19:14:57 +0200

    installation script

--- _install/install.sh
+++ _install/install.sh
@@ -1,73 +1,174 @@
-# Terminal utils
-echo "\nInstalling terminal utils\n"
-paru -S --needed 
+#!/bin/sh
 
-# Desktop
-echo "\nInstalling hyprland...\n"
-paru -S --needed 
+start_dir=$(pwd)
+script_dir="$(cd "$(dirname "$0")" && pwd)"
 
-echo "\nInstalling costumization apps...\n"
-paru -S --needed 
+mkdir -p "$HOME/Documents"
+mkdir -p "$HOME/Downloads"
+mkdir -p "$HOME/Games"
+mkdir -p "$HOME/Pictures"
+mkdir -p "$HOME/Pictures/screenshots"
+mkdir -p "$HOME/Projects"
+mkdir -p "$HOME/Videos"
+mkdir -p "$HOME/.local/share/gnupg"
+mkdir -p "$HOME/.local/bin"
 
-read -p "Do you want Flatpak support? [y/N] " flatpaks
-flatpaks=$(echo "$flatpaks" | tr '[:upper:]' '[:lower:]')
-
-if [ "$flatpaks" = "y" ]; then
-    paru -S --needed flatpak
-    flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-else
-    echo "Skipping..."
+# CHECK IF PARU IS INSTALLED
+if ! command -v paru >/dev/null 2>&1; then
+    echo "paru is not installed, installing..."
+    if git clone https://aur.archlinux.org/paru ~/Downloads/paru; then
+        if cd ~/Downloads/paru; then
+            makepkg -si
+            cd "$start_dir"
+        else
+            echo "Failed to change directory to ~/Downloads/paru"
+            exit 1
+        fi
+    else
+        echo "Failed to clone paru repository"
+        exit 1
+    fi
 fi
 
-read -p "Do you want AppImage support? [y/N] " appimages
-appimages=$(echo "$appimages" | tr '[:upper:]' '[:lower:]')
+echo "\nUpdating system...\n"
+paru -Syu
 
-if [ "$appimages" = "y" ]; then
-    paru -S --needed fuse2
+# ENABLE MULTILIB
+multilib_pattern="^\[multilib\]$"
+include_pattern="^Include = /etc/pacman.d/mirrorlist$"
+
+# Check if the exact lines exist in /etc/pacman.conf
+if grep -Fxq "[multilib]" /etc/pacman.conf && grep -Fxq "Include = /etc/pacman.d/mirrorlist" /etc/pacman.conf; then
+    echo "Both [multilib] and Include = /etc/pacman.d/mirrorlist are present in /etc/pacman.conf."
 else
-    echo "Skipping..."
+    echo "Required lines are not present in /etc/pacman.conf. Appending them to the end of the file."
+
+    # Use sudo to append the exact lines at the end of /etc/pacman.conf
+    sudo sh -c '{
+        echo "[multilib]"
+        echo "Include = /etc/pacman.d/mirrorlist"
+    } >> /etc/pacman.conf'
+
+    echo "Updating repositories..."
+    paru -Syu
 fi
 
-read -p "Do you want snap support? [y/N] " snaps
+create_symlink() {
+    src=$1
+    tgt=$2
+    current_date=$(date +"%Y%m%d_%H%M%S") # Format the current date
 
-echo "Just kidding..."
+    # Check if source directory exists
+    if [ -d "$src" ]; then
+        # Create target directory if it doesn't exist
+        if [ -e "$tgt" ]; then
+            # Rename existing directory
+            mv "$tgt" "${tgt}_bak_$current_date"
+            echo "Renamed existing directory: $tgt to ${tgt}_bak_$current_date"
+        fi
 
-read -p "Do you want QEMU/KVM support? [y/N] " qemuvm
-qemuvm=$(echo "$qemuvm" | tr '[:upper:]' '[:lower:]')
+        mkdir -p "$tgt" # Create the target directory
+        link_path="$tgt/$(basename "$src")"
 
-if [ "$qemuvm" = "y" ]; then
-    paru -S --needed 
-else
-    echo "Skipping..."
+        # Create the symlink if it doesn't already exist
+        if [ ! -e "$link_path" ]; then
+            ln -s "$src" "$link_path"
+            echo "Created symlink: $link_path -> $src"
+        else
+            echo "Symlink already exists: $link_path"
+        fi
+    else
+        echo "Source directory does not exist: $src"
+    fi
+}
+
+ask() {
+    read -p "$1 [y/N]: " res
+    res=$(echo "$res" | tr '[:upper:]' '[:lower:]')
+    local -n ref=$2
+    ref=false
+    if [ "$res" = "y" ]; then
+        ref=true
+    else
+        ref=false
+    fi
+}
+
+packages=""
+
+read -p "What GPU do you have? [1] AMD / [2] Nvidia / [3] Intel / [0] None: " gpu
+case "$gpu" in
+1)
+    echo "You have selected AMD GPU."
+    packages+="$(cat "$script_dir/packages/gpu-amd.txt")"
+    ;;
+2)
+    echo "You have selected Nvidia GPU."
+    packages+="$(cat "$script_dir/packages/gpu-nvidia.txt")"
+    ;;
+3)
+    echo "You have selected Intel GPU."
+    packages+="$(cat "$script_dir/packages/gpu-intel.txt")"
+    ;;
+*)
+    echo "No GPU selected, skiping drivers..."
+    ;;
+esac
+
+packages+="$(cat "$script_dir/packages/audio.txt")"
+packages+="$(cat "$script_dir/packages/terminal.txt")"
+packages+="$(cat "$script_dir/packages/coding.txt")"
+packages+="$(cat "$script_dir/packages/hyprland.txt")"
+packages+="$(cat "$script_dir/packages/bluetooth.txt")"
+packages+=" flatpak "
+packages+=" fuse2 "
+packages+="$(cat "$script_dir/packages/extras.txt")"
+
+vm=false
+ask "Do you want QEMU/KVM support?" vm
+if "$vm"; then
+    packages+="$(cat "$script_dir/packages/virt.txt")"
 fi
 
-read -p "Do you want Bluetooth support? [y/N] " blue
-blue=$(echo "$blue" | tr '[:upper:]' '[:lower:]')
+sudo ln -s /usr/bin/foot /usr/bin/xdg-terminal-exec # Set foot as xdg exec terminal
+chsh -s /usr/bin/zsh                                # Change default shell to zsh
 
-if [ "$blue" = "y" ]; then
-    paru -S --needed 
+zshexport='export ZDOTDIR="$HOME/.config/zsh"'
+if grep -Fxq "$zshexport" /etc/zsh/zshenv; then
+    echo "export already done"
 else
-    echo "Skipping..."
+    echo "/etc/zsh/zshenv does not point to home/.config/zsh"
+    sudo mkdir -p /etc/zsh
+    sudo sh -c "echo '$zshexport' >> /etc/zsh/zshenv"
 fi
 
-echo "Linking all configs..."
+if [ -n "$packages" ]; then
+    echo "Installing packages: $packages"
+    sudo paru -S --needed $packages
+fi
 
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install $(cat "$script_dir/packages/flatpaks.txt" | xargs)
 
-echo "Copying scripts"
-git clone https://github.com/m4rti21/tmux-sessionizer ~/Projects/tmux-sessionizer
-git clone https://github.com/m4rti21/ctpv ~/Projects/ctpv
-git clone https://github.com/m4rti21/compi.nvim ~/Projects/compi.nvim
-git clone https://github.com/m4rti21/acc ~/Projects/acc
+target_dir="$HOME/.config"
+mkdir -p "$target_dir"
+# Loop through each item in the $start_dir/config directory
+for dir in "$start_dir/config"/*; do
+    dir_name=$(basename "$dir")
+    symlink_path="$target_dir/$dir_name"
+    if [ ! -e "$symlink_path" ]; then
+        ln -s "$dir" "$symlink_path"
+        echo "Created symlink: $symlink_path -> $dir"
+    else
+        echo "Symlink already exists: $symlink_path"
+    fi
+done
 
-create_symlink "$HOME/Projects/acc/acc" "$HOME/.local/bin"
-create_symlink "$HOME/Projects/tmux-sessionizer/tmux-sessionizer" "$HOME/.local/bin"
-
-create_symlink "$HOME/Projects/ctpv/ctpv" "$HOME/.local/bin"
-create_symlink "$HOME/Projects/ctpv/ctpvclear" "$HOME/.local/bin"
-
-cd "$HOME/Projects/ctpv"
-make
-
-cd "$start_dir"
+create_symlink "$start_dir/icons" "$HOME/.local/share"
+create_symlink "$start_dir/icons" "$HOME/.icons"
+create_symlink "$start_dir/themes" "$HOME/.local/share"
+create_symlink "$start_dir/themes" "$HOME/.themes"
+create_symlink "$start_dir/fonts" "$HOME/.local/share"
+create_symlink "$start_dir/wallpapers" "$HOME/Pictures/wallpapers"
 
 echo "All done!"
