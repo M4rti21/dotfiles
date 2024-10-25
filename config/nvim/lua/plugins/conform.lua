3375630fd3c14fc7834aecef305b9a7b96b75a27
M4rti21 <m4rti21@proton.me>
2024-10-25 15:30:02 +0200

    format

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -20,10 +20,12 @@ return {
 				json = { "prettierd" },
 				sh = { "shfmt" },
 			},
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = true,
-			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
 		})
 		vim.opt.formatexpr = "v:lua.require('conform').formatexpr()"
 	end,
