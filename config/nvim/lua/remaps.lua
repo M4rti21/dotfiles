d834c80efab9e6ce7082309c4f0c5e91b5baf958
M4rti21 <m4rti21@proton.me>
2024-10-30 22:58:16 +0100

    idk

--- config/nvim/lua/remaps.lua
+++ config/nvim/lua/remaps.lua
@@ -35,3 +35,4 @@ vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Cmod [x] current file" })
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("i", "<D-Space>", "")
