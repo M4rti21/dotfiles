90ec02067afe80f0c6c438cd354a80354272b1e4
M4rti21 <m4rti21@proton.me>
2024-11-12 09:53:43 +0100

    stuff

--- config/nvim/lua/remaps.lua
+++ config/nvim/lua/remaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pn", vim.cmd.Ex, { desc = "[P]roject [N]etrw" })
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "[P]roject [N]etrw" })
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
