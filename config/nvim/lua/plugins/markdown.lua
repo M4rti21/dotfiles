d6592f0c2cbfbd1042ae93ca312a5fd4d36b1489
M4rti21 <m4rti21@proton.me>
2024-11-13 12:30:06 +0100

    tex

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,10 +1,5 @@
 return {
 	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-		opts = {},
-	},
-	{
 		"toppair/peek.nvim",
 		event = { "VeryLazy" },
 		build = "deno task --quiet build:fast",
