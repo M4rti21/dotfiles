0fcbea934db6b98c777dde9a0159f8cccbd6e90e
M4rti21 <m4rti21@proton.me>
2024-11-13 18:31:00 +0100

    add spotify

--- config/hypr/scripts/scratchpads.sh
+++ config/hypr/scripts/scratchpads.sh
@@ -8,7 +8,7 @@ toggle_scratchpad() {
     workspace_name="$1"
     cmd="$2"
 
-    windows=$( windows_in "$workspace_name" )
+    windows=$(windows_in "$workspace_name")
     if [ -z "$windows" ]; then
         hyprctl dispatch "exec [workspace special:$workspace_name] $cmd"
     else
@@ -20,6 +20,6 @@ if [ "$1" = "volume" ]; then
     toggle_scratchpad "volume" "pavucontrol"
 elif [ "$1" = "password" ]; then
     toggle_scratchpad "password" 'flatpak run com.bitwarden.desktop'
-# elif [ "$1" = "music" ]; then
-#     toggle_scratchpad "music" "LD_PRELOAD=~/Projects/spotifywm/spotifywm.so flatpak run com.spotify.Client"
+elif [ "$1" = "music" ]; then
+    toggle_scratchpad "music" """flatpak run --file-forwarding --command=sh com.spotify.Client -c 'eval "$(sed s#LD_PRELOAD=#LD_PRELOAD=$HOME/.local/share/spotify-adblock/spotify-adblock.so:#g /app/bin/spotify)"' @@u %U @@"""
 fi
