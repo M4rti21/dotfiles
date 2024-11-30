c6b992873046c83d3afad0be4a612b58da9e794a
M4rti21 <m4rti21@proton.me>
2024-11-30 01:38:38 +0100

    down

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,8 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-	},
+	-- {
+	-- 	"MeanderingProgrammer/render-markdown.nvim",
+	-- 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	-- },
 	{
 		"toppair/peek.nvim",
 		build = "deno task --quiet build:fast",
--- config/zsh/.zlogin
+++ config/zsh/.zlogin
@@ -2,5 +2,7 @@
 if [[ "$(tty)" = "/dev/tty1" ]]; then
     Hyprland &> /dev/null
 elif [[ "$(tty)" = "/dev/tty2" ]]; then
+    dwl
+elif [[ "$(tty)" = "/dev/tty3" ]]; then
     startx
 fi
