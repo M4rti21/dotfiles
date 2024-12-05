cfc4484663cb81f6b976038e93f4469c141bbc7c
M4rti21 <m4rti21@proton.me>
2024-12-05 13:02:33 +0100

    a

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -21,8 +21,8 @@ return {
 				throttle_at = 200000, -- start throttling when file exceeds this
 				throttle_time = "auto", -- minimum amount of time in milliseconds
 			})
-			-- vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
-			-- vim.api.nvim_create_user_command("PeekClose", peek.close, {})
+			vim.api.nvim_create_user_command("PeekOpen", peek.open, {})
+			vim.api.nvim_create_user_command("PeekClose", peek.close, {})
 		end,
 	},
 }
