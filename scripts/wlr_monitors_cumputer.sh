3a61a0accbb11fa3707169e0c6a230d25ff536f6
M4rti21 <m4rti21@proton.me>
2024-12-11 00:14:38 +0100

    blocks

--- scripts/wlr_monitors_cumputer.sh
+++ scripts/wlr_monitors_cumputer.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wlr-randr --output DP-1 --mode 1920x1080@359.747986Hz --pos 0,0 --adaptive-sync enabled
+wlr-randr --output HDMI-A-2 --off
 # wlr-randr --output DP-1 --mode 1920x1080@359.747986Hz --pos 1080,420 --adaptive-sync enabled
 # wlr-randr --output DP-2 --mode 1920x1080@60Hz --pos 3000,420
 # wlr-randr --output HDMI-A-2 --mode 1920x1080@60Hz --pos 0,0 --transform 90
