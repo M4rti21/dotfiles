1f153032e664dc8981996673d7d84dd0bd1dcb26
M4rti21 <m4rti21@proton.me>
2024-12-23 00:13:36 +0100

    i dont even know

---
+++ cmus/.config/cmus/lib.pl
@@ -0,0 +1,154 @@
+/home/m4rti/Nextcloud/Music/Ace of Base - All That She Wants.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - Death.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - The Sun, The Moon, The Star.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - Goodbye.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - Hunger.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - Guardian.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - One Hollow Word.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - Slave to the Riff.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - Cycle.mp3
+/home/m4rti/Nextcloud/Music/Æther Realm - Craft And The Creator.mp3
+/home/m4rti/Nextcloud/Music/AJR - Burn the House Down.mp3
+/home/m4rti/Nextcloud/Music/AJR - Wow, I'm Not Crazy.mp3
+/home/m4rti/Nextcloud/Music/Alestorm - Keelhauled.mp3
+/home/m4rti/Nextcloud/Music/Alestorm - Shipwrecked.mp3
+/home/m4rti/Nextcloud/Music/Alex C., Yass - Doktorspiele - Official Nightcore Version 2024.mp3
+/home/m4rti/Nextcloud/Music/Alex Gaudino, Crystal Waters - Destination Calabria - Radio Edit.mp3
+/home/m4rti/Nextcloud/Music/Alice Deejay - Better Off Alone.mp3
+/home/m4rti/Nextcloud/Music/AronChupa, Little Sis Nora - Little Swing.mp3
+/home/m4rti/Nextcloud/Music/Arrested Youth - The Kid I Used to Know.mp3
+/home/m4rti/Nextcloud/Music/Avenged Sevenfold - Buried Alive.mp3
+/home/m4rti/Nextcloud/Music/Avenged Sevenfold - Save Me.mp3
+/home/m4rti/Nextcloud/Music/Babalos - Snow Crystal.mp3
+/home/m4rti/Nextcloud/Music/BABY GRAVY, Yung Gravy, bbno$ - Super Smash Bros..mp3
+/home/m4rti/Nextcloud/Music/BABY GRAVY, Yung Gravy, bbno$ - Goodness Gracious.mp3
+/home/m4rti/Nextcloud/Music/bbno$, Rich Brian - edamame.mp3
+/home/m4rti/Nextcloud/Music/bbno$ - bag or die.mp3
+/home/m4rti/Nextcloud/Music/bbno$ - lil' freak.mp3
+/home/m4rti/Nextcloud/Music/Billy Talent - Fallen Leaves.mp3
+/home/m4rti/Nextcloud/Music/Blaumut - Pa amb Oli I Sal.mp3
+/home/m4rti/Nextcloud/Music/Bo Burnham - White Woman’s Instagram.mp3
+/home/m4rti/Nextcloud/Music/Bo Burnham - Welcome to The Internet.mp3
+/home/m4rti/Nextcloud/Music/Bo Burnham - 1985.mp3
+/home/m4rti/Nextcloud/Music/Bring Me The Horizon - Can You Feel My Heart.mp3
+/home/m4rti/Nextcloud/Music/Buhos - Volcans.mp3
+/home/m4rti/Nextcloud/Music/Buhos - Transmets Energia.mp3
+/home/m4rti/Nextcloud/Music/Calvin Harris - Summer.mp3
+/home/m4rti/Nextcloud/Music/Caramella Girls - Caramelldansen.mp3
+/home/m4rti/Nextcloud/Music/Connor Kauffman - Bleed.mp3
+/home/m4rti/Nextcloud/Music/Connor Price, bbno$ - Not A Beanie.mp3
+/home/m4rti/Nextcloud/Music/Coolio, L.V. - Gangsta's Paradise.mp3
+/home/m4rti/Nextcloud/Music/Doctor Prats - Les Nits No Moren Mai.mp3
+/home/m4rti/Nextcloud/Music/Doctor Prats - Caminem Lluny.mp3
+/home/m4rti/Nextcloud/Music/Doctor Prats, Stay Homas - Seguirem Ballant.mp3
+/home/m4rti/Nextcloud/Music/Doctor Prats - Massa Bé.mp3
+/home/m4rti/Nextcloud/Music/Dynazty - Natural Born Killer.mp3
+/home/m4rti/Nextcloud/Music/Eiffel 65, Gabry Ponte - Blue (Da Ba Dee) - Gabry Ponte Video Edit.mp3
+/home/m4rti/Nextcloud/Music/Electric Callboy - Everytime We Touch - TEKKNO Version.mp3
+/home/m4rti/Nextcloud/Music/Els Catarres - Vull estar amb tu.mp3
+/home/m4rti/Nextcloud/Music/Eminem - Without Me.mp3
+/home/m4rti/Nextcloud/Music/Ethan Bortnick - engravings.mp3
+/home/m4rti/Nextcloud/Music/Fallis - Epic Sax.mp3
+/home/m4rti/Nextcloud/Music/Fall Out Boy - The Phoenix.mp3
+/home/m4rti/Nextcloud/Music/Fall Out Boy - Centuries.mp3
+/home/m4rti/Nextcloud/Music/Felsenstein - Go Go Go Go! - Nightcore Version.mp3
+/home/m4rti/Nextcloud/Music/Figa Flawas - MUSSEGU.mp3
+/home/m4rti/Nextcloud/Music/Figa Flawas, Mushkaa - DIABLA.mp3
+/home/m4rti/Nextcloud/Music/Flashy Ice Cream - Quan em tornes a mirar.mp3
+/home/m4rti/Nextcloud/Music/Fools Garden - Lemon Tree.mp3
+/home/m4rti/Nextcloud/Music/Funk Tribu - Phonky Tribu.mp3
+/home/m4rti/Nextcloud/Music/George Ezra - Green Green Grass.mp3
+/home/m4rti/Nextcloud/Music/Gigi D'Agostino - The Riddle.mp3
+/home/m4rti/Nextcloud/Music/Gloryhammer - Hootsforce.mp3
+/home/m4rti/Nextcloud/Music/Gossos - Corren.mp3
+/home/m4rti/Nextcloud/Music/Gotye, Kimbra - Somebody That I Used To Know.mp3
+/home/m4rti/Nextcloud/Music/Green Day - American Idiot.mp3
+/home/m4rti/Nextcloud/Music/Han Solo, Stalker - Mandragora Filipina - Live Mix.mp3
+/home/m4rti/Nextcloud/Music/Imagine Dragons - Bones.mp3
+/home/m4rti/Nextcloud/Music/Infected Mushroom - Becoming Insane.mp3
+/home/m4rti/Nextcloud/Music/Infected Mushroom - Change the Formality.mp3
+/home/m4rti/Nextcloud/Music/Infected Mushroom - Saeed.mp3
+/home/m4rti/Nextcloud/Music/Infected Mushroom - Pink Nightmares.mp3
+/home/m4rti/Nextcloud/Music/Infected Mushroom - The Pretender.mp3
+/home/m4rti/Nextcloud/Music/Infected Mushroom, Hatikva 6 - Where Do I Belong.mp3
+/home/m4rti/Nextcloud/Music/Infected Mushroom - Guitarmass.mp3
+/home/m4rti/Nextcloud/Music/ItaloBrothers - Stamp On The Ground.mp3
+/home/m4rti/Nextcloud/Music/Joan Dausà - Jo Mai Mai.mp3
+/home/m4rti/Nextcloud/Music/Kelemen Kabátban, Eckü - Maradjatok Gyerekek.mp3
+/home/m4rti/Nextcloud/Music/Korpiklaani - Ievan Polkka.mp3
+/home/m4rti/Nextcloud/Music/Layto, Neoni, Arcando - Ghost Town - Arcando Remix.mp3
+/home/m4rti/Nextcloud/Music/Lildami, Sr. Chen - 2080.mp3
+/home/m4rti/Nextcloud/Music/Lildami, Killah Man - 0822.mp3
+/home/m4rti/Nextcloud/Music/Lildami, Sr. Chen, Xavibo - Mediterrani.mp3
+/home/m4rti/Nextcloud/Music/Lildami - M'és igual.mp3
+/home/m4rti/Nextcloud/Music/Lildami, Triquell - 4 gats.mp3
+/home/m4rti/Nextcloud/Music/Linkin Park - In the End.mp3
+/home/m4rti/Nextcloud/Music/Linkin Park - Numb.mp3
+/home/m4rti/Nextcloud/Music/Linkin Park - Breaking the Habit.mp3
+/home/m4rti/Nextcloud/Music/Little Big, bbno$ - It Happens (feat. bbno$).mp3
+/home/m4rti/Nextcloud/Music/Lou Bega - Mambo No. 5 (a Little Bit of...).mp3
+/home/m4rti/Nextcloud/Music/Luiza Lian - Tucum.mp3
+/home/m4rti/Nextcloud/Music/Mandragora - Galactic Symphony.mp3
+/home/m4rti/Nextcloud/Music/Måneskin - I WANNA BE YOUR SLAVE.mp3
+/home/m4rti/Nextcloud/Music/Marcus & Martinus, bbno$ - We Are Not The Same.mp3
+/home/m4rti/Nextcloud/Music/Masked Wolf - Astronaut In The Ocean.mp3
+/home/m4rti/Nextcloud/Music/meganeko - Feral.mp3
+/home/m4rti/Nextcloud/Music/Metalocalypse- Dethklok - Awaken.mp3
+/home/m4rti/Nextcloud/Music/Modjo - Lady - Hear Me Tonight.mp3
+/home/m4rti/Nextcloud/Music/Mushkaa, Calagher, roots - Barras Warras.mp3
+/home/m4rti/Nextcloud/Music/Mystic Prophecy - I'm Still Standing.mp3
+/home/m4rti/Nextcloud/Music/OCT - Don't Touch My Clogs.mp3
+/home/m4rti/Nextcloud/Music/Oques Grasses - Sta guai.mp3
+/home/m4rti/Nextcloud/Music/Oques Grasses, ZOO, Lildami - Més Likes.mp3
+/home/m4rti/Nextcloud/Music/Oques Grasses - Petar-ho.mp3
+/home/m4rti/Nextcloud/Music/OSI - Radiologue.mp3
+/home/m4rti/Nextcloud/Music/O-Zone - Dragostea din tei.mp3
+/home/m4rti/Nextcloud/Music/Pierce The Veil, Kellin Quinn - King For A Day.mp3
+/home/m4rti/Nextcloud/Music/Poor Man's Poison - Feed the Machine.mp3
+/home/m4rti/Nextcloud/Music/Post Malone - Circles.mp3
+/home/m4rti/Nextcloud/Music/Post Malone, Ozzy Osbourne, Travis Scott - Take What You Want (feat. Ozzy Osbourne & Travis Scott).mp3
+/home/m4rti/Nextcloud/Music/Post Malone, Morgan Wallen - I Had Some Help (Feat. Morgan Wallen).mp3
+/home/m4rti/Nextcloud/Music/Powerwolf - Incense & Iron.mp3
+/home/m4rti/Nextcloud/Music/Rare Americans - Brittle Bones Nicky.mp3
+/home/m4rti/Nextcloud/Music/RichaadEB, Cristina Vee - Bad Apple!!.mp3
+/home/m4rti/Nextcloud/Music/Robert Miles - Children.mp3
+/home/m4rti/Nextcloud/Music/S3RL - Catchit.mp3
+/home/m4rti/Nextcloud/Music/Sabaton - The Last Stand.mp3
+/home/m4rti/Nextcloud/Music/Sangtraït - El Museu dels Arbres Morts.mp3
+/home/m4rti/Nextcloud/Music/Sangtraït - El Vol de L'Home Ocell - Del Disc Els Senyors de Les Pedres.mp3
+/home/m4rti/Nextcloud/Music/Scatman John - Scatman's World - Club Mix.mp3
+/home/m4rti/Nextcloud/Music/Set It Off - Hourglass.mp3
+/home/m4rti/Nextcloud/Music/Set It Off - Skeleton.mp3
+/home/m4rti/Nextcloud/Music/Set It Off - Projector.mp3
+/home/m4rti/Nextcloud/Music/SIAMES - The Wolf.mp3
+/home/m4rti/Nextcloud/Music/Skillet - Feel Invincible.mp3
+/home/m4rti/Nextcloud/Music/STARSET - My Demons.mp3
+/home/m4rti/Nextcloud/Music/Stay Homas - LA NÒRIA.mp3
+/home/m4rti/Nextcloud/Music/Tally Hall - The Bidding.mp3
+/home/m4rti/Nextcloud/Music/The Tyets - Menorca.mp3
+/home/m4rti/Nextcloud/Music/The Tyets - Coti x Coti.mp3
+/home/m4rti/Nextcloud/Music/The Tyets - Olívia.mp3
+/home/m4rti/Nextcloud/Music/The Tyets - Desembre.mp3
+/home/m4rti/Nextcloud/Music/The Tyets - Bomba de fum.mp3
+/home/m4rti/Nextcloud/Music/The Weeknd - Blinding Lights.mp3
+/home/m4rti/Nextcloud/Music/The Weeknd - Save Your Tears.mp3
+/home/m4rti/Nextcloud/Music/Timmy Trumpet, Savage - Freaks.mp3
+/home/m4rti/Nextcloud/Music/Tom Walker, Rxseboy, Foster, Masked Wolf, Sarcastic Sounds - Something Beautiful (feat. Rxseboy, Foster, Masked Wolf & Sarcastic Sounds).mp3
+/home/m4rti/Nextcloud/Music/Tones And I - Dance Monkey.mp3
+/home/m4rti/Nextcloud/Music/Triquell - Jugular.mp3
+/home/m4rti/Nextcloud/Music/Twenty One Pilots - Stressed Out.mp3
+/home/m4rti/Nextcloud/Music/Twenty One Pilots - Ride.mp3
+/home/m4rti/Nextcloud/Music/Twenty One Pilots - Chlorine.mp3
+/home/m4rti/Nextcloud/Music/Two Door Cinema Club - Undercover Martyn.mp3
+/home/m4rti/Nextcloud/Music/Hommarju - Masterpiece.mp3
+/home/m4rti/Nextcloud/Music/Aaron Smith, Luvli, Krono - Dancin - Krono Remix.mp3
+/home/m4rti/Nextcloud/Music/Shaggy - Mr Boombastic.mp3
+/home/m4rti/Nextcloud/Music/Wind Rose - Diggy Diggy Hole.mp3
+/home/m4rti/Nextcloud/Music/Xandria - Reborn.mp3
+/home/m4rti/Nextcloud/Music/Ylvis - You're Fucked.mp3
+/home/m4rti/Nextcloud/Music/Yolanda Be Cool, DCup - We No Speak Americano (Edit).mp3
+/home/m4rti/Nextcloud/Music/Yung Gravy, bbno$, Rich Brian - C’est La Vie (with bbno$ & Rich Brian).mp3
+/home/m4rti/Nextcloud/Music/Zac Bnjmn - Delta.mp3
+/home/m4rti/Nextcloud/Music/Zamir, marc indigo, Chevy - on the move.mp3
+/home/m4rti/Nextcloud/Music/暁Records - KARMANATIONS.mp3
+/home/m4rti/Nextcloud/Music/理芽 - 法螺話.mp3
